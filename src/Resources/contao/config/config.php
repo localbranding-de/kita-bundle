2f2235c32fe656c75ecba9b8c2a1525bcb009b92
Frank Jeroma <53892311+localbranding-de@users.noreply.github.com>
2022-09-05 10:17:58 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,18 +4,7 @@
  * Hooks.
  */
 
-if (TL_MODE == 'BE') {
 
-}
-
-
-if ('BE' === TL_MODE) {
-
-     $GLOBALS['TL_CSS'][] = '/bundles/bedesign/css/lb_be_default.css';
-        $GLOBALS['TL_CSS'][] = '/bundles/bedesign/css/lb_be_basic.css';
-
-    
-}
 
 
 
